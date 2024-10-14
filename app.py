cbb1101a65b95abe2d43ca90178c2309fe83b621
67070151 <67070151@kmitl.ac.th>
2024-10-14 21:50:41 +0700

    Add login function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/muscal'
@@ -9,5 +11,24 @@ db = SQLAlchemy(app)
 class LoginInfo(db.Model):
     __tablename__ = 'login_info'
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = LoginInfo.query.filter_by(username=username).first()
+
+    if user:
+        if check_password_hash(user.password, password):
+            return jsonify({'message': 'Login successful!'}), 200
+        else:
+            return jsonify({'message': 'Invalid password'}), 401
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
